15b8cfd188dc9bb814e45275ee995f1b09829ebb
spikespaz <spikespaz@outlook.com>
2017-02-06 10:33:34 -0700

    Made ReferenceStrings useful

--- src/main/java/com/spikespaz/essentialadditions/Main/MainRegistry.java
+++ src/main/java/com/spikespaz/essentialadditions/Main/MainRegistry.java
@@ -1,6 +1,7 @@
 package com.spikespaz.essentialadditions.Main;
 
 import com.spikespaz.essentialadditions.blocks.ModBlocks;
+import com.spikespaz.essentialadditions.lib.ReferenceStrings;
 import com.spikespaz.essentialadditions.world.ModWorld;
 import com.spikespaz.essentialadditions.items.ModItems;
 import cpw.mods.fml.common.Mod;
@@ -10,10 +11,10 @@ import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
 
-@Mod(modid="essentialadditions", name="Essential Additions", version="1.2.8")
+@Mod(modid=ReferenceStrings.MODID, name=ReferenceStrings.NAME, version=ReferenceStrings.VERSION)
 public class MainRegistry
 {
-  @SidedProxy(clientSide="com.spikespaz.essentialadditions.Main.ClientProxy", serverSide="com.spikespaz.essentialadditions.Main.ServerProxy")
+  @SidedProxy(clientSide=ReferenceStrings.CLIENTSIDE, serverSide=ReferenceStrings.SERVERSIDE)
   public static ServerProxy proxy;
 
   @Mod.EventHandler
--- src/main/java/com/spikespaz/essentialadditions/lib/ReferenceStrings.java
+++ src/main/java/com/spikespaz/essentialadditions/lib/ReferenceStrings.java
@@ -5,6 +5,6 @@ public class ReferenceStrings
   public static final String MODID = "essentialadditions";
   public static final String NAME = "Essential Additions";
   public static final String VERSION = "1.2.8";
-  public static final String CLIENTSIDE = "com.spikespaz.essentialadditions.MainClientProxy";
-  public static final String SERVERSIDE = "com.spikespaz.essentialadditions.MainServerProxy";
+  public static final String CLIENTSIDE = "com.spikespaz.essentialadditions.Main.ClientProxy";
+  public static final String SERVERSIDE = "com.spikespaz.essentialadditions.Main.ServerProxy";
 }
