a6d835377435f172688ff91c4ce60c73d36726c5
spikespaz <spikespaz@outlook.com>
2017-02-07 04:55:41 -0700

    fixed access

--- src/main/java/com/spikespaz/essentialadditions/EssentialAdditions.java
+++ src/main/java/com/spikespaz/essentialadditions/EssentialAdditions.java
@@ -11,11 +11,11 @@ import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = EssentialAdditions.MODID, name = EssentialAdditions.NAME, version = EssentialAdditions.VERSION)
 public class EssentialAdditions {
-    public static final String MODID = "essentialadditions";
-    public static final String NAME = "Essential Additions";
-    public static final String VERSION = "1.3.0";
-    public static final String CLIENTSIDE = "com.spikespaz.essentialadditions.Proxy.ClientProxy";
-    public static final String SERVERSIDE = "com.spikespaz.essentialadditions.Proxy.CommonProxy";
+    static final String MODID = "essentialadditions";
+    static final String NAME = "Essential Additions";
+    static final String VERSION = "1.3.0";
+    private static final String CLIENTSIDE = "com.spikespaz.essentialadditions.Proxy.ClientProxy";
+    private static final String SERVERSIDE = "com.spikespaz.essentialadditions.Proxy.CommonProxy";
 
     @SidedProxy(clientSide = EssentialAdditions.CLIENTSIDE, serverSide = EssentialAdditions.SERVERSIDE)
     public static CommonProxy proxy;
