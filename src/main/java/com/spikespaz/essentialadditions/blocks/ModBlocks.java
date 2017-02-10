257a44066982e350ecc5f45a9b8307e6910bd203
thvardhan <thvardhan.god@gmail.com>
2017-02-11 00:06:17 +0530

    Revert "Added drops to EyeOre."
    This reverts commit ad71c07343fa3188a2a4ca8ba0c2812ab01b9250.

--- src/main/java/com/spikespaz/essentialadditions/blocks/ModBlocks.java
+++ src/main/java/com/spikespaz/essentialadditions/blocks/ModBlocks.java
@@ -5,7 +5,11 @@ import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.boss.EntityDragon;
+import net.minecraft.init.Items;
 import net.minecraft.item.Item;
+import net.minecraft.world.IBlockAccess;
 
 import java.util.Random;
 
@@ -32,7 +36,15 @@ public class ModBlocks {
             }
         }.setStepSound(Block.soundTypeSand);
 
-        EyeOre = new EyeOre(Material.rock, "EyeOre", 3.0F, 5.0F, "pickaxe", 3, CreativeTabs.tabBlock);
+        EyeOre = new BaseBlock(Material.rock, "EyeOre", 3.0F, 5.0F, "pickaxe", 3, CreativeTabs.tabBlock) {
+            public Item getItemDropped(int par1, Random random, int par2) {
+                return Items.ender_pearl;                                  // This should eventually drop "Items.ender_pearl" and "Items.blaze_powder"
+            }
+
+            public boolean canEntityDestroy(IBlockAccess world, int x, int y, int z, Entity entity) {
+                return !(entity instanceof EntityDragon) || this != EyeOre;
+            }
+        };
 
         RubyBlock = new BaseBlock(Material.rock, "RubyBlock", 5.0F, 10.0F, "pickaxe", 1, CreativeTabs.tabBlock).setStepSound(Block.soundTypeMetal);
         RubyOre = new BaseBlock(Material.rock, "RubyOre", 3.0F, 5.0F, "pickaxe", 3, CreativeTabs.tabBlock) {
