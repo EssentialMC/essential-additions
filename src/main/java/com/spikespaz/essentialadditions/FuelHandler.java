4181ae6a6eca1333bf8c708d8faf9b94ba19ce4a
spikespaz <spikespaz@outlook.com>
2017-02-11 00:27:53 -0700

    Thvardhan likes his whitespace.

--- src/main/java/com/spikespaz/essentialadditions/FuelHandler.java
+++ src/main/java/com/spikespaz/essentialadditions/FuelHandler.java
@@ -6,28 +6,14 @@ import net.minecraft.block.Block;
 import net.minecraft.item.ItemStack;
 
 public class FuelHandler implements IFuelHandler {
-
-
     @Override
     public int getBurnTime(ItemStack itemStack) {
-
         Block blockFuel = Block.getBlockFromItem(itemStack.getItem());
-
-        if(blockFuel== ModBlocks.CharcoalBlock){
-            return 16000;//there are 20 ticks/sec in minecraft world. Coal block burns for 13min 20sec
-            //so 13*60+20=800sec. 800*20=16K. (in minecraft ticks)
+        // There are 20TPS in Minecraft. (S*20=T)
+        if(blockFuel == ModBlocks.CharcoalBlock) {
+            return 16000;
         }
-
-        /*
-        This handler runs in background of game. Whenever someone tries to use anything as a fuel
-        this function will be called. So I am checking if that block is CharcoalBlock, if it is,
-        then return the burn time which is same as Coal Block. else return 0; dont change that 0.
-        As changing it will cause every item in minecraft burn XD.
-         */
-
+        // Don't change this zero.
         return 0;
     }
-
-
-
 }
