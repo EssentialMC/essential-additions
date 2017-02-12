c269ff415166bb84051e3253b6a57b26c427fa7a
spikespaz <spikespaz@outlook.com>
2017-02-12 16:35:56 -0700

    Does java not have a pass?

--- src/main/java/com/spikespaz/essentialadditions/proxy/ClientProxy.java
+++ src/main/java/com/spikespaz/essentialadditions/proxy/ClientProxy.java
@@ -8,7 +8,6 @@ import net.minecraft.item.Item;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.common.registry.GameRegistry;
 
 import static com.spikespaz.essentialadditions.main.EssentialAdditions.MODID;
 
@@ -16,17 +15,17 @@ public class ClientProxy extends CommonProxy {
 
     @Override
     public void preInit(FMLPreInitializationEvent e) {
-        super.preInit(e);
+//        super.preInit(e);
     }
 
     @Override
     public void init(FMLInitializationEvent e) {
-        super.init(e);
+//        super.init(e);
     }
 
     @Override
     public void postInit(FMLPostInitializationEvent e) {
-        super.postInit(e);
+//        super.postInit(e);
     }
 
     // Override from CommonProxy to render stuff.
@@ -39,8 +38,9 @@ public class ClientProxy extends CommonProxy {
             Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(item, 0,
                     new ModelResourceLocation(MODID + ":" + item.getUnlocalizedName().substring(5), "inventory"));
         } else if (object instanceof Block) {
-            Block block = (Block) object;
-            GameRegistry.registerBlock(block, block.getUnlocalizedName());
+            String dummy = "dummy";
+//            Block block = (Block) object;
+//            GameRegistry.registerBlock(block, block.getUnlocalizedName());
         }
     }
 }
