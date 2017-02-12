8515bca63edd563f62edff6e1a4f0ab4107c6d5e
spikespaz <spikespaz@outlook.com>
2017-02-11 22:27:20 -0700

    Frmoved @thvardhan whitespace, again...

--- src/main/java/com/spikespaz/essentialadditions/blocks/CharcoalBlock.java
+++ src/main/java/com/spikespaz/essentialadditions/blocks/CharcoalBlock.java
@@ -4,17 +4,7 @@ import net.minecraft.block.material.Material;
 import net.minecraft.creativetab.CreativeTabs;
 
 public class CharcoalBlock extends BaseBlock {
-
-
     CharcoalBlock(Material material, String unlocalizedName, Float hardness, Float resistance, String tool, Integer mineLevel, CreativeTabs creativeTab) {
         super(material, unlocalizedName, hardness, resistance, tool, mineLevel, creativeTab);
     }
-
-
-
-
 }
-
-
-
-
