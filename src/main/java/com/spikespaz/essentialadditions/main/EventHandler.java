90fcb2453441995a3c0ea9c9198e328a0424679f
spikespaz <spikespaz@outlook.com>
2017-02-18 09:53:00 -0700

    Disabled code for spawner drops

--- src/main/java/com/spikespaz/essentialadditions/main/EventHandler.java
+++ src/main/java/com/spikespaz/essentialadditions/main/EventHandler.java
@@ -1,22 +1,16 @@
 package com.spikespaz.essentialadditions.main;
 
-import net.minecraft.block.BlockMobSpawner;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import net.minecraftforge.event.world.BlockEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-
-import static net.minecraft.block.Block.spawnAsEntity;
-
 public class EventHandler {
-    @SubscribeEvent
-    public void BreakEvent(BlockEvent.BreakEvent event) {
-        // Get tool used to break the block.
-        Item toolUsed = event.getPlayer().getCurrentEquippedItem().getItem();
-        // Drop itself when a mob spawner is broken.
-        if (event.state.getBlock() instanceof BlockMobSpawner && !event.world.isRemote && toolUsed.getHarvestLevel(new ItemStack(toolUsed), "pickaxe") >= 2) {
-            ItemStack drop = new ItemStack(Item.getItemFromBlock(event.state.getBlock()));
-            spawnAsEntity(event.world, event.pos, drop);
-        }
-    }
+//    @SubscribeEvent
+//    public void BreakEvent(BlockEvent.BreakEvent event) {
+//        // Get tool used to break the block.
+//        ItemStack toolUsed = event.getPlayer().getCurrentEquippedItem();
+//        // Drop itself when a mob spawner is broken.
+//        if (event.state.getBlock() instanceof BlockMobSpawner && !event.world.isRemote && toolUsed.getItem().getHarvestLevel(toolUsed, "pickaxe") >= 2) {
+//            TileEntity tile = event.world.getTileEntity(event.pos);
+//            ItemStack drop = new ItemStack(Item.getItemFromBlock(event.state.getBlock()));
+//            drop.setTagCompound(tile.getTileData());
+//            spawnAsEntity(event.world, event.pos, drop);
+//        }
+//    }
 }
