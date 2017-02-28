1db2f57d954271abc5abaa9de56304d4ac69fa60
spikespaz <spikespaz@outlook.com>
2017-02-28 06:02:43 -0700

    Remove broken rotation code

--- src/main/java/com/spikespaz/essentialadditions/tileentity/TileEntityEyeOreRenderer.java
+++ src/main/java/com/spikespaz/essentialadditions/tileentity/TileEntityEyeOreRenderer.java
@@ -3,7 +3,6 @@ package com.spikespaz.essentialadditions.tileentity;
 import com.spikespaz.essentialadditions.proxy.ClientProxy;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
-import net.minecraft.util.math.MathHelper;
 
 public class TileEntityEyeOreRenderer extends TileEntitySpecialRenderer<TileEntityEyeOre> {
     @Override
@@ -13,26 +12,8 @@ public class TileEntityEyeOreRenderer extends TileEntitySpecialRenderer<TileEnti
 
         this.bindTexture(ClientProxy.EYE_BALL_TEXTURE);
 
-        float f = te.tickCount + partialTicks;
-        GlStateManager.translate(0.0F, 0.1F + MathHelper.sin(f * 0.1F) * 0.01F, 0.0F);
-        float f1;
-
-        f1 = te.eyeRotation - te.eyeRotationPrev;
-        while (f1 >= Math.PI) {
-            f1 -= Math.PI * 2F;
-        }
-
-        while (f1 < -Math.PI) {
-            f1 += Math.PI * 2F;
-        }
-
-        float f2 = te.eyeRotationPrev + f1 * partialTicks;
-
-        GlStateManager.rotate((float) (-f2 * (180F / Math.PI)), 0.0F, 1.0F, 0.0F);
-        GlStateManager.rotate(80.0F, 0.0F, 0.0F, 1.0F);
-
         GlStateManager.enableCull();
-        ClientProxy.EYE_BALL_MODEL.render(null, f, 0, 0, 0, 0.0F, 0.0625F);
+        ClientProxy.EYE_BALL_MODEL.render(null, 0, 0, 0, 0, 0, 0.0625F);
 
         GlStateManager.popMatrix();
     }
