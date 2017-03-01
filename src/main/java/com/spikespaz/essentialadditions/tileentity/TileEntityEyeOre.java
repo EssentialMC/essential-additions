ad0f61b35e17113f23f98b60bef10bf4a67fac70
spikespaz <spikespaz@outlook.com>
2017-02-28 17:03:59 -0700

    Made random rotation reversal more natural

--- src/main/java/com/spikespaz/essentialadditions/tileentity/TileEntityEyeOre.java
+++ src/main/java/com/spikespaz/essentialadditions/tileentity/TileEntityEyeOre.java
@@ -4,10 +4,16 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ITickable;
 
+import static com.spikespaz.essentialadditions.proxy.ClientProxy.UNIVERSAL_RANDOM;
+import static java.lang.Math.abs;
+
 public class TileEntityEyeOre extends TileEntity implements ITickable {
     static float[] rotateTo;
-    public int delayedTickCount;
-    public EntityPlayer nearestPlayer;
+    int delayedTickCount;
+    private boolean reversed;
+    private float lastChanged;
+    Float idleRotation = 0F;
+    EntityPlayer nearestPlayer;
 
     @Override
     public void update() {
@@ -28,6 +34,18 @@ public class TileEntityEyeOre extends TileEntity implements ITickable {
             delayedTickCount = 0;
         } else {
             ++delayedTickCount;
+        }
+
+        // Randomly reverse the direction as long as it hasn't already been changed within 1.5 seconds ago.
+        if (UNIVERSAL_RANDOM.nextInt(50) == 0 & abs(delayedTickCount - lastChanged) > 30) {
+            reversed = !reversed;
+            lastChanged = delayedTickCount;
+        }
+
+        if (reversed) {
+            idleRotation += 3.6F;
+        } else {
+            idleRotation -= 3.6F;
         }
     }
 }
--- src/main/java/com/spikespaz/essentialadditions/tileentity/TileEntityEyeOreRenderer.java
+++ src/main/java/com/spikespaz/essentialadditions/tileentity/TileEntityEyeOreRenderer.java
@@ -4,27 +4,26 @@ import com.spikespaz.essentialadditions.proxy.ClientProxy;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
 
-import static com.spikespaz.essentialadditions.proxy.ClientProxy.UNIVERSAL_RANDOM;
 import static java.lang.Math.abs;
 
 public class TileEntityEyeOreRenderer extends TileEntitySpecialRenderer<TileEntityEyeOre> {
-    Float multiplier = 3.6F;
-
     @Override
     public void renderTileEntityAt(TileEntityEyeOre te, double x, double y, double z, float partialTicks, int destroyStage) {
         GlStateManager.pushMatrix();
         GlStateManager.translate(x + 0.5F, y + 0.5F, z + 0.5F);
 
-        if (te.nearestPlayer == null) {
-            if (te.delayedTickCount == 100 && UNIVERSAL_RANDOM.nextBoolean()) {
-                multiplier = -multiplier;
-            }
-
-            GlStateManager.rotate((multiplier * te.delayedTickCount), 0, 1, 0);
+        // If the player exists watch them.
+        if (te.nearestPlayer != null) {
+            GlStateManager.rotate(te.rotateTo[0], 1, 0, 0);
+            GlStateManager.rotate(te.rotateTo[1], 0, 1, 0);
+            GlStateManager.rotate(te.rotateTo[2], 0, 0, 1);
+        } else {
+            // If there is no close enough player just scan the surrounding terrain.
+            // Randomly reverses direction of rotation.
+            GlStateManager.rotate(te.idleRotation, 0, 1, 0);
 
             double smoothFloat = abs((te.delayedTickCount - 50) / 400D) - 0.0625;
             GlStateManager.translate(0, smoothFloat, 0);
-        } else {
         }
 
         GlStateManager.enableCull();
